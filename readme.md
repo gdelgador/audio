d3170272973ddbab1f94a331321289a1313dba2c
gdelgador <gonzalo.delgado.r@uni.com>
2021-10-12 00:02:59 -0500

    web-search

---
+++ readme.md
@@ -0,0 +1,5 @@
+https://chromedriver.chromium.org/downloads
+
+
+pipwin install pyaudio
+# https://stackoverflow.com/questions/61348555/error-pyaudio-0-2-11-cp38-cp38-win-amd64-whl-is-not-a-supported-wheel-on-this-p
--- voice.py
+++ voice.py
@@ -1,20 +1,40 @@
-import re
-import speech_recognition
+import speech_recognition 
 
-# Obtain audio from the microphone
-recognizer = speech_recognition.Recognizer()
-with speech_recognition.Microphone() as source:
-    print("Say something!")
-    audio = recognizer.listen(source)
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
-# Recognize speech using Google Speech Recognition
-words = recognizer.recognize_google(audio)
 
-# Respond to speech
-print(words)
+def main():
+    # search words
+    recognizer = speech_recognition.Recognizer()
+    
+    with speech_recognition.Microphone() as source:
+        print("Say something!")
+        audio = recognizer.listen(source)
 
-# matches = re.search("my name is (.*)", words)
-# if matches:
-#     print(f"Hey, {matches[1]}.")
-# else:
-#     print("Hey, you.")
+    # Recognize speech using Google Speech Recognition
+    words = recognizer.recognize_google(audio, language="es-PE")
+    print(words)
+
+    time.sleep(4)
+
+    # init browser
+    driver = webdriver.Chrome("./drivers/chromedriver.exe")
+    url="http://www.google.com"
+    driver.get(url)
+
+    time.sleep(4)
+
+    search = driver.find_element_by_name('q')
+    search.send_keys(words)
+    search.send_keys(Keys.ENTER)
+
+    time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
